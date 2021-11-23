2b18f5940b96f189c0c4169e24f04a1ae7124836
ivoronline <ivoronline@gmail.com>
2021-11-23 20:00:20 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_db_findermethods_projections/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_findermethods_projections/controllers/MyController.java
@@ -1,6 +1,6 @@
 package com.ivoronline.springboot_db_findermethods_projections.controllers;
 
-import com.ivoronline.springboot_db_findermethods_projections.entities.PersonView;
+import com.ivoronline.springboot_db_findermethods_projections.entities.PersonProjection;
 import com.ivoronline.springboot_db_findermethods_projections.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+  //PROPERTIES
   @Autowired PersonRepository personRepository;
 
   //================================================================
   // FIND PERSON VIEW BY NAME
   //================================================================
   @RequestMapping("FindPersonViewByName")
-  PersonView findPersonViewByName() {
-    PersonView personView = personRepository.findPersonViewByName("Bill");
-    return     personView;
+  PersonProjection findPersonViewByName() {
+    PersonProjection personProjection = personRepository.findPersonViewByName("Bill");
+    return personProjection;
   }
 
 }
--- src/main/java/com/ivoronline/springboot_db_findermethods_projections/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_findermethods_projections/repositories/PersonRepository.java
@@ -1,11 +1,11 @@
 package com.ivoronline.springboot_db_findermethods_projections.repositories;
 
 import com.ivoronline.springboot_db_findermethods_projections.entities.Person;
-import com.ivoronline.springboot_db_findermethods_projections.entities.PersonView;
+import com.ivoronline.springboot_db_findermethods_projections.entities.PersonProjection;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
-  PersonView findPersonViewByName(String name);
+  PersonProjection findPersonViewByName(String name);
 }
 
 
